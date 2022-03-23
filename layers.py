696418818baef0c2d3ca04c2351e8e9f30985569
sunbowen11 <sunbowen11@jd.com>
2022-03-23 14:36:04 +0800

    runable PNN model v1

---
+++ layers.py
@@ -0,0 +1,348 @@
+import itertools
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+class DNN(nn.Module):
+    """The Multi Layer Percetron
+
+      Input shape
+        - nD tensor with shape: ``(batch_size, ..., input_dim)``. The most common situation would be a 2D input with shape ``(batch_size, input_dim)``.
+
+      Output shape
+        - nD tensor with shape: ``(batch_size, ..., hidden_size[-1])``. For instance, for a 2D input with shape ``(batch_size, input_dim)``, the output would have shape ``(batch_size, hidden_size[-1])``.
+
+      Arguments
+        - **inputs_dim**: input feature dimension.
+
+        - **hidden_units**:list of positive integer, the layer number and units in each layer.
+
+        - **activation**: Activation function to use.
+
+        - **l2_reg**: float between 0 and 1. L2 regularizer strength applied to the kernel weights matrix.
+
+        - **dropout_rate**: float in [0,1). Fraction of the units to dropout.
+
+        - **use_bn**: bool. Whether use BatchNormalization before activation or not.
+
+        - **seed**: A Python integer to use as random seed.
+    """
+
+    def __init__(self, inputs_dim, hidden_units, activation='relu', l2_reg=0, dropout_rate=0, use_bn=False,
+                 init_std=0.0001, dice_dim=3, seed=1024, device='cpu'):
+        super(DNN, self).__init__()
+        self.dropout_rate = dropout_rate
+        self.dropout = nn.Dropout(dropout_rate)
+        self.seed = seed
+        self.l2_reg = l2_reg
+        self.use_bn = use_bn
+        if len(hidden_units) == 0:
+            raise ValueError("hidden_units is empty!!")
+        hidden_units = [inputs_dim] + list(hidden_units)
+
+        self.linears = nn.ModuleList(
+            [nn.Linear(hidden_units[i], hidden_units[i + 1]) for i in range(len(hidden_units) - 1)])
+
+        if self.use_bn:
+            self.bn = nn.ModuleList(
+                [nn.BatchNorm1d(hidden_units[i + 1]) for i in range(len(hidden_units) - 1)])
+
+        self.activation_layers = nn.ModuleList(
+            [activation_layer(activation, hidden_units[i + 1], dice_dim) for i in range(len(hidden_units) - 1)])
+
+        for name, tensor in self.linears.named_parameters():
+            if 'weight' in name:
+                nn.init.normal_(tensor, mean=0, std=init_std)
+
+        self.to(device)
+
+    def forward(self, inputs):
+        deep_input = inputs
+
+        for i in range(len(self.linears)):
+
+            fc = self.linears[i](deep_input)
+
+            if self.use_bn:
+                fc = self.bn[i](fc)
+
+            fc = self.activation_layers[i](fc)
+
+            fc = self.dropout(fc)
+            deep_input = fc
+        return deep_input
+
+
+def concat_fun(inputs, axis=-1):
+    if len(inputs) == 1:
+        return inputs[0]
+    else:
+        return torch.cat(inputs, dim=axis)
+
+
+class InnerProductLayer(nn.Module):
+    """InnerProduct Layer used in PNN that compute the element-wise
+    product or inner product between feature vectors.
+      Input shape
+        - a list of 3D tensor with shape: ``(batch_size,1,embedding_size)``.
+      Output shape
+        - 3D tensor with shape: ``(batch_size, N*(N-1)/2 ,1)`` if use reduce_sum. or 3D tensor with shape:
+        ``(batch_size, N*(N-1)/2, embedding_size )`` if not use reduce_sum.
+      Arguments
+        - **reduce_sum**: bool. Whether return inner product or element-wise product
+      References
+            - [Qu Y, Cai H, Ren K, et al. Product-based neural networks for user response prediction[C]//
+            Data Mining (ICDM), 2016 IEEE 16th International Conference on. IEEE, 2016: 1149-1154.]
+            (https://arxiv.org/pdf/1611.00144.pdf)"""
+
+    def __init__(self, reduce_sum=True, device='cpu'):
+        super(InnerProductLayer, self).__init__()
+        self.reduce_sum = reduce_sum
+        self.to(device)
+
+    def forward(self, inputs):
+
+        embed_list = inputs
+        row = []
+        col = []
+        num_inputs = len(embed_list)
+
+        for i in range(num_inputs - 1):
+            for j in range(i + 1, num_inputs):
+                row.append(i)
+                col.append(j)
+        p = torch.cat([embed_list[idx]
+                       for idx in row], dim=1)  # batch num_pairs k
+        q = torch.cat([embed_list[idx]
+                       for idx in col], dim=1)
+
+        inner_product = p * q
+        if self.reduce_sum:
+            inner_product = torch.sum(
+                inner_product, dim=2, keepdim=True)
+        return inner_product
+
+
+class OutterProductLayer(nn.Module):
+    """OutterProduct Layer used in PNN.This implemention is
+    adapted from code that the author of the paper published on https://github.com/Atomu2014/product-nets.
+      Input shape
+            - A list of N 3D tensor with shape: ``(batch_size,1,embedding_size)``.
+      Output shape
+            - 2D tensor with shape:``(batch_size,N*(N-1)/2 )``.
+      Arguments
+            - **filed_size** : Positive integer, number of feature groups.
+            - **kernel_type**: str. The kernel weight matrix type to use,can be mat,vec or num
+            - **seed**: A Python integer to use as random seed.
+      References
+            - [Qu Y, Cai H, Ren K, et al. Product-based neural networks for user response prediction[C]//Data Mining (ICDM), 2016 IEEE 16th International Conference on. IEEE, 2016: 1149-1154.](https://arxiv.org/pdf/1611.00144.pdf)
+    """
+
+    def __init__(self, field_size, embedding_size, kernel_type='mat', seed=1024, device='cpu'):
+        super(OutterProductLayer, self).__init__()
+        self.kernel_type = kernel_type
+
+        num_inputs = field_size
+        num_pairs = int(num_inputs * (num_inputs - 1) / 2)
+        embed_size = embedding_size
+        if self.kernel_type == 'mat':
+
+            self.kernel = nn.Parameter(torch.Tensor(
+                embed_size, num_pairs, embed_size))
+
+        elif self.kernel_type == 'vec':
+            self.kernel = nn.Parameter(torch.Tensor(num_pairs, embed_size))
+
+        elif self.kernel_type == 'num':
+            self.kernel = nn.Parameter(torch.Tensor(num_pairs, 1))
+        nn.init.xavier_uniform_(self.kernel)
+
+        self.to(device)
+
+    def forward(self, inputs):
+        embed_list = inputs
+        row = []
+        col = []
+        num_inputs = len(embed_list)
+        for i in range(num_inputs - 1):
+            for j in range(i + 1, num_inputs):
+                row.append(i)
+                col.append(j)
+        p = torch.cat([embed_list[idx]
+                       for idx in row], dim=1)  # batch num_pairs k
+        q = torch.cat([embed_list[idx] for idx in col], dim=1)
+
+        # -------------------------
+        if self.kernel_type == 'mat':
+            p.unsqueeze_(dim=1)
+            # k     k* pair* k
+            # batch * pair
+            kp = torch.sum(
+
+                # batch * pair * k
+
+                torch.mul(
+
+                    # batch * pair * k
+
+                    torch.transpose(
+
+                        # batch * k * pair
+
+                        torch.sum(
+
+                            # batch * k * pair * k
+
+                            torch.mul(
+
+                                p, self.kernel),
+
+                            dim=-1),
+
+                        2, 1),
+
+                    q),
+
+                dim=-1)
+        else:
+            # 1 * pair * (k or 1)
+
+            k = torch.unsqueeze(self.kernel, 0)
+
+            # batch * pair
+
+            kp = torch.sum(p * q * k, dim=-1)
+
+            # p q # b * p * k
+
+        return kp
+
+
+class PredictionLayer(nn.Module):
+    """
+      Arguments
+         - **task**: str, ``"binary"`` for  binary logloss or  ``"regression"`` for regression loss
+         - **use_bias**: bool.Whether add bias term or not.
+    """
+
+    def __init__(self, task='binary', use_bias=True, **kwargs):
+        if task not in ["binary", "multiclass", "regression"]:
+            raise ValueError("task must be binary,multiclass or regression")
+
+        super(PredictionLayer, self).__init__()
+        self.use_bias = use_bias
+        self.task = task
+        if self.use_bias:
+            self.bias = nn.Parameter(torch.zeros((1,)))
+
+    def forward(self, X):
+        output = X
+        if self.use_bias:
+            output += self.bias
+        if self.task == "binary":
+            output = torch.sigmoid(output)
+        return output
+
+
+
+def activation_layer(act_name, hidden_size=None, dice_dim=2):
+    """Construct activation layers
+
+    Args:
+        act_name: str or nn.Module, name of activation function
+        hidden_size: int, used for Dice activation
+        dice_dim: int, used for Dice activation
+    Return:
+        act_layer: activation layer
+    """
+    if isinstance(act_name, str):
+        if act_name.lower() == 'sigmoid':
+            act_layer = nn.Sigmoid()
+        # elif act_name.lower() == 'linear':
+        #     act_layer = Identity()
+        elif act_name.lower() == 'relu':
+            act_layer = nn.ReLU(inplace=True)
+        # elif act_name.lower() == 'dice':
+        #     assert dice_dim
+        #     act_layer = Dice(hidden_size, dice_dim)
+        elif act_name.lower() == 'prelu':
+            act_layer = nn.PReLU()
+    elif issubclass(act_name, nn.Module):
+        act_layer = act_name()
+    else:
+        raise NotImplementedError
+
+    return act_layer
+
+
+class SequencePoolingLayer(nn.Module):
+    """The SequencePoolingLayer is used to apply pooling operation(sum,mean,max) on variable-length sequence feature/multi-value feature.
+
+      Input shape
+        - A list of two  tensor [seq_value,seq_len]
+
+        - seq_value is a 3D tensor with shape: ``(batch_size, T, embedding_size)``
+
+        - seq_len is a 2D tensor with shape : ``(batch_size, 1)``,indicate valid length of each sequence.
+
+      Output shape
+        - 3D tensor with shape: ``(batch_size, 1, embedding_size)``.
+
+      Arguments
+        - **mode**:str.Pooling operation to be used,can be sum,mean or max.
+
+    """
+
+    def __init__(self, mode='mean', supports_masking=False, device='cpu'):
+
+        super(SequencePoolingLayer, self).__init__()
+        if mode not in ['sum', 'mean', 'max']:
+            raise ValueError('parameter mode should in [sum, mean, max]')
+        self.supports_masking = supports_masking
+        self.mode = mode
+        self.device = device
+        self.eps = torch.FloatTensor([1e-8]).to(device)
+        self.to(device)
+
+    def _sequence_mask(self, lengths, maxlen=None, dtype=torch.bool):
+        # Returns a mask tensor representing the first N positions of each cell.
+        if maxlen is None:
+            maxlen = lengths.max()
+        row_vector = torch.arange(0, maxlen, 1).to(lengths.device)
+        matrix = torch.unsqueeze(lengths, dim=-1)
+        mask = row_vector < matrix
+
+        mask.type(dtype)
+        return mask
+
+    def forward(self, seq_value_len_list):
+        if self.supports_masking:
+            uiseq_embed_list, mask = seq_value_len_list  # [B, T, E], [B, 1]
+            mask = mask.float()
+            user_behavior_length = torch.sum(mask, dim=-1, keepdim=True)
+            mask = mask.unsqueeze(2)
+        else:
+            uiseq_embed_list, user_behavior_length = seq_value_len_list  # [B, T, E], [B, 1]
+            mask = self._sequence_mask(user_behavior_length, maxlen=uiseq_embed_list.shape[1],
+                                       dtype=torch.float32)  # [B, 1, maxlen]
+            mask = torch.transpose(mask, 1, 2)  # [B, maxlen, 1]
+
+        embedding_size = uiseq_embed_list.shape[-1]
+
+        mask = torch.repeat_interleave(mask, embedding_size, dim=2)  # [B, maxlen, E]
+
+        if self.mode == 'max':
+            hist = uiseq_embed_list - (1 - mask) * 1e9
+            hist = torch.max(hist, dim=1, keepdim=True)[0]
+            return hist
+        hist = uiseq_embed_list * mask.float()
+        hist = torch.sum(hist, dim=1, keepdim=False)
+
+        if self.mode == 'mean':
+            self.eps = self.eps.to(user_behavior_length.device)
+            hist = torch.div(hist, user_behavior_length.type(torch.float32) + self.eps)
+
+        hist = torch.unsqueeze(hist, dim=1)
+        return hist
